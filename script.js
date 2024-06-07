b79c4fd0ae860e4787d22db6f487f33852c8d721
Gabrielly pereira da silva <110669243+Gabrijalda@users.noreply.github.com>
2024-06-07 11:24:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaprincipal= document.querySelector(".caixa-principal");
+const caixaperguntas= document.querySelector(".caixa-perguntas");
+const caixaalternativas= document.querySelector(".caixa-alternativas");
+const caixaresultado= document.querySelector(".caixa-resultado");
+const textoresultado= document.querySelector(".textoresultado");
