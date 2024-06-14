b684716ccdc9502bc82ed04cecb04d487db5a60f
Gabrielly pereira da silva <110669243+Gabrijalda@users.noreply.github.com>
2024-06-14 12:08:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const caixaprincipal= document.querySelector(".caixa-principal");
-const caixaperguntas= document.querySelector(".caixa-perguntas");
-const caixaalternativas= document.querySelector(".caixa-alternativas");
-const caixaresultado= document.querySelector(".caixa-resultado");
-const textoresultado= document.querySelector(".textoresultado");
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoresultado = document.querySelector(".textoresultado");
+
+const perguntas = [
+    {
+        anuciado: "assim que saiu da escola voce se depara com uma nova tecnologia",
+        alternativas: [
+            "alternativa 1"],
+        "aternativa 2"
+    ]
+}
+{
+    anuciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1"],
+            "aternativa 2"
+    ]
+};
+{
+    {
+        anuciado: "pergunta 1",
+            alternativas: [
+                "alternativa 1"],
+                "aternativa 2"
+    },
+    {
+        anuciado: "pergunta 1",
+            alternativas: [
+                "alternativa 1"],
+                "aternativa 2"
+            ]
+        },
+        {
+            anuciado: "pergunta 1",
+                alternativas: [
+                    "alternativa 1"],
+                    "aternativa 2"
+                ]        
